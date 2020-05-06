a45dea3e310d55a43dacab9f6120bf280451480d
dilettalagom <dilettalagom@gmail.com>
2020-05-06 18:29:44 +0200

    docker-compose: singoli nodi attivi TODO: comunicazione hdfs m-w

--- docker-compose/target-master/start-services-master.sh
+++ docker-compose/target-master/start-services-master.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
+: ${HADOOP_PREFIX:=/usr/local/hadoop}
+sudo $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+
+rm /tmp/*.pid
+service ssh start
+
 hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
-hdfs dfs -chmod -R 777 /
-$HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
+hdfs dfs -chmod -R 777 /
 hdfs dfs -mkdir /spark-events
 hdfs dfs -mkdir /spark-logs
 mkdir /tmp/spark-events
 
-sleep 30
+#sleep 30
 
 $SPARK_HOME/sbin/start-all.sh
 $SPARK_HOME/sbin/start-history-server.sh
 
+/bin/bash
