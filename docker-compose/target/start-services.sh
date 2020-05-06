2a06d4ec3bc0cf6901f9262e8e5d3e01dde19fa6
dilettalagom <dilettalagom@gmail.com>
2020-05-06 16:17:59 +0200

    Env building 1

--- docker-compose/target/start-services.sh
+++ docker-compose/target/start-services.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
+hdfs dfs -chmod -R 777 /
 $HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
-hdfs dfs -chmod -R 777 /
 hdfs dfs -mkdir /spark-events
 hdfs dfs -mkdir /spark-logs
+mkdir /tmp/spark-events
 
 sleep 30
 
 $SPARK_HOME/sbin/start-all.sh
 $SPARK_HOME/sbin/start-history-server.sh
+
