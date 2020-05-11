c05a96664c1658d58820070eab8d5120d5a753ea
dilettalagom <dilettalagom@gmail.com>
2020-05-12 01:23:48 +0200

    Q1_pre_processing: funzionante (nifi+codice python) TODO:aggiungere la validazione

--- docker-images/nifi-node/nifi-scripts/pre_processing_nat.py
+++ docker-images/nifi-node/nifi-scripts/pre_processing_nat.py
@@ -1,40 +1,10 @@
-#import java.io
-#from org.apache.commons.io import IOUtils
-#from java.nio.charset import StandardCharsets
-from org.apache.nifi.processor.io import StreamCallback
+#!/usr/bin/env python3
+
+import sys
+#sys.path.append('/usr/local/lib/python2.7/dist-packages')
 import pandas as pd
 
-
-# Define a subclass of StreamCallback for use in session.write()
-class PyStreamCallback(StreamCallback):
-    def __init__(self):
-        pass
-
-    def process(self, inputStream, outputStream):
-
-        #input_data = IOUtils.toString(inputStream, StandardCharsets.UTF_8)  # .split('\n')
-        # df1 = csv.reader(input_data, delimiter=',')
-        # dataset_nation = pd.read_csv(inputStream, StandardCharsets.UTF_8)
-
-        df1 = pd.read_csv(inputStream)
-        df2 = df1[['dimessi_guariti', 'tamponi']].shift(periods=1, fill_value=0)
-        df1[['dimessi_guariti', 'tamponi']] = df1[['dimessi_guariti', 'tamponi']].subtract(
-            df2[['dimessi_guariti', 'tamponi']])
-
-        result = df1.to_csv(flowFile.getAttribute('filename'), sep=',', index=False)
-        if result is None:
-            print('No matching found.')
-        else:
-            outputStream.write(bytearray(result.encode('utf-8')))
-# end class
-
-flowFile = session.get()
-if flowFile != None:
-    flowFile = session.putAttribute(flowFile, flowFile.getAttribute('filename'))
-    flowFile = session.write(flowFile, PyStreamCallback())
-    session.transfer(flowFile, REL_SUCCESS)
-else:
-    session.transfer(flowFile, REL_FAILURE)
-session.commit()
-
-#./data/nifi-scripts/pre_proccessing_nat.py
+df1 = pd.read_csv(sys.stdin)
+df2 = df1[['dimessi_guariti', 'tamponi']].shift(periods=1, fill_value=0)
+df1[['dimessi_guariti', 'tamponi']] = df1[['dimessi_guariti', 'tamponi']].subtract(df2[['dimessi_guariti', 'tamponi']])
+df1.to_csv(sys.stdout,index=False)
