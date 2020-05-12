7e46d22b7261388dbdc8ad6f4aff53e2ef7718c0
giomarc <xkiryax@gmail.com>
2020-05-12 10:50:40 +0200

    nifi flow query 1 finally working!

--- docker-images/nifi-node/nifi-scripts/pre_processing_nat.sh
+++ docker-images/nifi-node/nifi-scripts/pre_processing_nat.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import sys
-#sys.path.append('/usr/local/lib/python2.7/dist-packages')
+sys.path.append('/usr/local/lib/python2.7/dist-packages')
 import pandas as pd
-
 df1 = pd.read_csv(sys.stdin)
 df2 = df1[['dimessi_guariti', 'tamponi']].shift(periods=1, fill_value=0)
 df1[['dimessi_guariti', 'tamponi']] = df1[['dimessi_guariti', 'tamponi']].subtract(df2[['dimessi_guariti', 'tamponi']])
