7555645e506a618f9af001b19756c6892e3fa888
giomarc <xkiryax@gmail.com>
2020-05-17 12:32:28 +0200

    query 2: nifi finally working

--- docker-compose/nifi-scripts/normalize_data_2.sh
+++ docker-compose/nifi-scripts/normalize_data_2.sh
@@ -15,23 +15,28 @@ header_subset = list(subset.columns.values)
 
 sub = subset.to_numpy()
 
-#find indexes of elements not in monotonic order
-indexes_to_rectify = np.argwhere((sub[:, 1:] >= sub[:, :-1]) == False)
+# find indexes of elements not in monotonic order
+indexes_to_rectify = np.argwhere((sub[:, 1:] >= sub[:, :-1]) == False)    
+#per sapere quante righe correggere eseguire  print(len(indexes_to_rectify))
 
-#substitute wrong element with mean of prev and next value
+# substitute wrong element with mean of prev and next value
 for i in indexes_to_rectify:
-    #sub[i[0]][i[1]] = math.ceil((sub[i[0]][i[1] + 1] - sub[i[0]][i[1] - 1])/2)
-    sub[i[0],i[1]] = sub[i[0],i[1] - 1]
+    indexes_to_rectify_in_row = np.argwhere((sub[i[0], i[1]:] < sub[i[0], i[1]]) == True)
 
-#data normalization
-#diff = np.diff(sub)
+    # finestra di numero di valori che accettiamo siano errati (come se fosse avvenuto un bitflip) = 10
+    #if len(indexes_to_rectify_in_row) <= 10:
+    for j in indexes_to_rectify_in_row:
+        sub[i[0], i[1] + j] = sub[i[0], i[1]]
+    #else:
+    #    print(len(indexes_to_rectify_in_row), all_line)
 
-#rebuild csv
-##total_diff = np.concatenate((sub[:, :1], diff), axis=1)
-#diff_df = pd.DataFrame(data=(total_diff[0:, 0:]), columns=header_subset)
-#total_data_df = pd.concat([geo_infos, continent, diff_df], axis=1, join='inner')
+# data normalization
+diff = np.diff(sub)
 
-tot = pd.DataFrame(data=(sub[0:, 0:]), columns=header_subset)
-total_data_df = pd.concat([geo_infos, continent, tot], axis=1, join='inner')
+# rebuild csv
+total_diff_np = np.concatenate((sub[:, :1], diff), axis=1)
+total_diff_df = pd.DataFrame(data=(total_diff_np[0:, 0:]), columns=header_subset)
+total_data_df = pd.concat([geo_infos, continent, total_diff_df], axis=1, join='inner')
 
+# write back
 total_data_df.to_csv(sys.stdout, index=False)
