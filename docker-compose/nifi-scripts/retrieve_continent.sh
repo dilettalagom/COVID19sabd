63d4c30ad7c669857f1dd171be8571e59579def3
giomarc <xkiryax@gmail.com>
2020-05-17 19:50:47 +0200

    no more quotes problem

---
+++ docker-compose/nifi-scripts/retrieve_continent.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import collections, csv, logging, os, sys, zipfile
+sys.path.append('/usr/local/lib/python2.7/dist-packages')
+import pycountry_convert as pc
+import pandas as pd
+import numpy as np
+import re
+import continetlib.retrieve_continent_lib as c
+
+# get input
+input = (sys.stdin).readline().replace('\n','')
+#remove comma between quotes
+input = re.sub(r'(")([^",]+),([^"]+)(")', r'\2 \3', input)
+#split
+line = input.split(',')
+
+# retrieve continent from class continentlib.Continent
+try:
+	line[len(line)-1] = c.get_continent(float(line[2]), float(line[3]))
+except ValueError:
+	pass
+
+with(sys.stdout):
+	writer = csv.writer(sys.stdout)
+	writer.writerow(line)
