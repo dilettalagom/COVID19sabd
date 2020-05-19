c3c7911032e5badd21dc33b5560bb50713e2c6be
giomarc <xkiryax@gmail.com>
2020-05-19 22:21:10 +0200

    deleted comment on KeyAccumulator

--- src/main/java/query/customCombiner/KeyAccumulator.java
+++ src/main/java/query/customCombiner/KeyAccumulator.java
@@ -8,93 +8,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public class KeyAccumulator implements Serializable {
 
 
     static WeekyearContinentComparator customComparator = new WeekyearContinentComparator();
 
-
-  /*  public Function<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>>,
-            List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>>>> createAccumulator() {
-        Function< Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> ,
-                List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>>>
-                > createAcc =
-                new Function<
-                        Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> ,
-                        List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >
-                        >() {
-                    @Override
-                    public List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >
-                    call(Tuple2<ClassificationKeyPojo,Tuple2<String, Double>> tuple) {
-                        return new ArrayList<>();
-                    }
-                };
-
-        return createAcc;
-    }
-
-
-    public Function2< List< Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> > ,
-            Tuple2<ClassificationKeyPojo, Tuple2<String, Double>>,
-            List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>>>
-            >createMergeOneValueAcc() {
-
-        Function2< List< Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> > ,
-                Tuple2<ClassificationKeyPojo, Tuple2<String, Double>>,
-                List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>>>
-                >mergeOneValueAcc =
-                new Function2< List< Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> > ,
-                        Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> ,
-                        List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >
-                        >(){
-                    @Override
-                    public List< Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >
-                    call(List< Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> > tuple1,
-                         Tuple2<ClassificationKeyPojo, Tuple2<String, Double> > element_tuple2) throws Exception {
-
-                        tuple1.add(element_tuple2);
-
-                        //Using custom-combiner
-                        Collections.sort(tuple1, customComparator.reversed());
-
-                        return tuple1;
-                    }
-                };
-        return mergeOneValueAcc;
-
-
-    }
-
-
-    public Function2< List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >,
-            List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >,
-            List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >
-            > createMergeObjectsAcc() {
-
-        Function2< List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >,
-                List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >,
-                List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >
-                > mergeObjectsAcc =
-                new Function2<List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >,
-                        List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >,
-                        List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >
-                        >() {
-                    @Override
-                    public List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> >
-                    call(List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>> > originTuple,
-                         List<Tuple2<ClassificationKeyPojo, Tuple2<String, Double>>> newTuple) throws Exception {
-
-                        originTuple.addAll(newTuple);
-
-                        //Using custom-combiner
-                        Collections.sort(originTuple, customComparator.reversed());
-
-                        return originTuple;
-                    }
-                };
-        return mergeObjectsAcc;
-
-    }*/
 
     public  Function<Tuple2<String, Double>, List<Tuple2<String, Double>>> createAccumulator() {
         Function<Tuple2<String, Double>, List<Tuple2<String, Double>>> createAcc = new Function<Tuple2<String, Double>, List<Tuple2<String, Double>>>() {
@@ -103,9 +22,9 @@ public class KeyAccumulator implements Serializable {
                 return new ArrayList<>();
             }
         };
-
         return createAcc;
     }
+
 
     public Function2<List<Tuple2<String, Double>>, Tuple2<String, Double>, List<Tuple2<String, Double>>> createMergeOneValueAcc() {
         Function2<List<Tuple2<String, Double>>, Tuple2<String, Double>, List<Tuple2<String, Double>>>
@@ -121,6 +40,7 @@ public class KeyAccumulator implements Serializable {
 
     }
 
+
     public Function2<List<Tuple2<String, Double>>, List<Tuple2<String, Double>>, List<Tuple2<String, Double>>> createMergeObjectsAcc() {
 
         Function2<List<Tuple2<String, Double>>,List<Tuple2<String, Double>>,List<Tuple2<String, Double>>> mergeObjectsAcc
@@ -132,7 +52,6 @@ public class KeyAccumulator implements Serializable {
                 return v1;
             }
         };
-
         return mergeObjectsAcc;
     }
 
