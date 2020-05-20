0596072f2ded08690f5612b50d588af113b67023
dilettalagom <dilettalagom@gmail.com>
2020-05-20 18:41:44 +0200

    flow.xml.gz solo preprocessing -> corretto

--- docker-compose/nifi-scripts/pre_processing_nat.sh
+++ docker-compose/nifi-scripts/pre_processing_nat.sh
@@ -3,7 +3,14 @@
 import sys
 sys.path.append('/usr/local/lib/python2.7/dist-packages')
 import pandas as pd
+
+
+#get input
 df1 = pd.read_csv(sys.stdin)
+
+#normalize data
 df2 = df1[['dimessi_guariti', 'tamponi']].shift(periods=1, fill_value=0)
 df1[['dimessi_guariti', 'tamponi']] = df1[['dimessi_guariti', 'tamponi']].subtract(df2[['dimessi_guariti', 'tamponi']])
+
+#send back
 df1.to_csv(sys.stdout,index=False)
